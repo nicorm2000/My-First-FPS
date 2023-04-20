a99f0e03c4ed66b4a74f4541693a562198c07cee
nicorm <nicorm2000@gmail.com>
2023-04-20 18:46:58 -0300

    Added custom gravity for the player (this will let me customize the fall of the player and due to the fact that i have a characcter controller it wwill behave better with stairs)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,9 +11,18 @@ public class Movement : MonoBehaviour
 
     [SerializeField] float gravity = -30f; //-9.81f 
     Vector3 verticalVelocity = Vector3.zero;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded;
 
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(transform.position, 0.1f, groundMask);
+
+        if (isGrounded)
+        {
+            verticalVelocity.y = 0;
+        }
+
         Vector3 horizontalVelocity = (transform.right * horizointalInput.x + transform.forward * horizointalInput.y) * speed;
 
         controller.Move(horizontalVelocity * Time.deltaTime);
