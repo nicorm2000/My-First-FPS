baf2d258cf5417b856a5d8633bad0aeeb7e6f2d0
nicorm <nicorm2000@gmail.com>
2023-04-20 12:00:42 -0300

    Started setting up the code, fixed how the players is in unity and added a feet pivot

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Setup")] [SerializeField] private Rigidbody rigidBody;
+
+    [SerializeField] private Transform feetPivot;
+
+    [Header("Movement")] [SerializeField] private float movementSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
